eb3e11df9e585d467d5ada0a67120c8b4b95676e
Byun SoYun <bsy1482@naver.com>
2024-05-22 14:05:01 +0900

    🔥

---
+++ Python/programmers/깊이_너비 우선 탐색(DFS_BFS)/43162_네트워크/solution.py
@@ -0,0 +1,21 @@
+import collections
+
+
+def solution(n, computers):
+    count = 0
+    visited = [False for _ in range(n)]
+    q = collections.deque()
+
+    while False in visited:
+        q.append(visited.index(False))
+        while q:
+            i = q.popleft()
+            visited[i] = True
+
+            for j in range(n):
+                if not visited[j] and j != i  and computers[i][j] == 1:
+                    q.append(j)
+
+        count += 1
+
+    return count

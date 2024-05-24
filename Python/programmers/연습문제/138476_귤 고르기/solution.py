aab936e712eee7583b425693320683200c38c8f4
Byun SoYun <bsy1482@naver.com>
2024-05-24 14:29:52 +0900

    🔥

---
+++ Python/programmers/연습문제/138476_귤 고르기/solution.py
@@ -0,0 +1,18 @@
+def solution(k, tangerine):
+    tanger_dic = {}
+    for t in tangerine:
+        tanger_dic[t] = tanger_dic.get(t, 0) + 1
+
+    sort_tanger_dic = sorted(tanger_dic.items(), key=lambda x: x[1], reverse=True)
+
+    count = 0
+    for ta in sort_tanger_dic:
+        count += 1
+        k -= ta[1]
+        if k <= 0:
+            break
+
+    return count
+
+# 문제
+# https://school.programmers.co.kr/learn/courses/30/lessons/138476

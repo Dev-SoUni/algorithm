7999bf2f61dc24b2624d9f77fc69373dce901685
Byun SoYun <bsy1482@naver.com>
2024-05-28 17:31:14 +0900

    🔥

---
+++ Python/programmers/131127_할인 행사/solution.py
@@ -0,0 +1,18 @@
+def solution(want, number, discount):
+    answer = 0
+    for l in range(len(discount)-9):
+        items = discount[l:l+10]
+        items_dic = {}
+        for it in items:
+            if it not in items_dic:
+                items_dic[it] = 1
+            else:
+                items_dic[it] += 1
+
+        for i in range(len(want)):
+            if want[i] not in items_dic or items_dic[want[i]] < number[i]:
+                break
+            if i == len(want) - 1:
+                answer += 1
+
+    return answer

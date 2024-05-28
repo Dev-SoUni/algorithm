9c0233a98a73bee931078e8a97d626e177c3e9e6
Byun SoYun <bsy1482@naver.com>
2024-05-28 17:36:25 +0900

    🔥

--- Python/programmers/131127_할인 행사/solution.py
+++ Python/programmers/131127_할인 행사/solution.py
@@ -16,3 +16,16 @@ def solution(want, number, discount):
                 answer += 1
 
     return answer
+
+from collections import Counter
+def solution2(want, number, discount):
+    answer = 0
+    dic = {}
+    for i in range(len(want)):
+        dic[want[i]] = number[i]
+
+    for i in range(len(discount)-9):
+        if dic == Counter(discount[i:i+10]):
+            answer += 1
+
+    return answer

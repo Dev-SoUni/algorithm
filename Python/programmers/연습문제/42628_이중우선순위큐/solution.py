9f5c400ba6ee84c3feb42063c64eef515ba64042
Byun SoYun <bsy1482@naver.com>
2024-05-30 17:21:42 +0900

    🔥

--- Python/programmers/연습문제/42628_이중우선순위큐/solution.py
+++ Python/programmers/연습문제/42628_이중우선순위큐/solution.py
@@ -1,18 +1,50 @@
+from heapq import heappush, heappop
+
+
 def solution(operations):
-    h = []
+    answer = []
+    max_heap = []
+    min_heap = []
 
-    for o in operations:
-        g, n = o.split()
-        if g == "I":
-            h.append(int(n))
-        elif h and n == "1":
-            h.sort()
-            h.pop()
-        elif h and n == "-1":
-            h.sort()
-            h.pop(0)
+    for operation in operations:
+        oper, num = operation.split()
+        num = int(num)
 
-    if not h:
+        if oper == "I":
+            node = [num, True]
+            heappush(max_heap, (-num, node))
+            heappush(min_heap, (num, node))
+        else:
+            if num == 1:
+                while len(max_heap) > 0:
+                    num, node = heappop(max_heap)
+                    val, vali = node
+                    if vali:
+                        node[1] = False
+                        break
+
+            elif num == -1:
+                while len(min_heap) > 0:
+                    num, node = heappop(min_heap)
+                    val, vali = node
+                    if vali:
+                        node[1] = False
+                        break
+
+    while len(max_heap) > 0:
+        num, node = heappop(max_heap)
+        val, vali = node
+        if vali:
+            answer.append(val)
+            break
+    if len(answer) == 0:
         return [0, 0]
-    return [max(h), min(h)]
 
+    while len(min_heap) > 0:
+        num, node = heappop(min_heap)
+        val, vali = node
+        if vali:
+            answer.append(val)
+            break
+
+    return answer

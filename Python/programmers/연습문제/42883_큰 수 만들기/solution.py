9c52872aa12ee91231d409402e22a684a1fd0349
Byun SoYun <bsy1482@naver.com>
2024-06-09 14:44:45 +0900

    🔥

--- Python/programmers/연습문제/42883_큰 수 만들기/solution.py
+++ Python/programmers/연습문제/42883_큰 수 만들기/solution.py
@@ -6,3 +6,20 @@ def solution(number, k):
             k -= 1
         answer.append(i)
     return ''.join(answer[:len(answer) - k])
+
+
+def solution2(people, limit):
+    people.sort()
+    answer = 0
+    while people:
+        print(people)
+        if people[0] + people[-1] <= limit:
+            people.pop(0)
+
+        answer += 1
+        people.pop()
+        print(f"people {people}")
+
+    return answer
+
+an = solution2([70, 50, 80, 50], 100)

6352a3467ab62cc1c195f76401c27219680c0d6d
Byun SoYun <bsy1482@naver.com>
2024-06-22 09:54:06 +0900

    🔥

---
+++ Python/programmers/84512_모음사전/solution.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+
+next_chars = {"A": "E", "E": "I", "I": "O", "O": "U", "U": "A"}
+
+
+def solution(word):
+    count = 1
+    chars = deque(["A"])
+
+    while word != "".join(chars):
+        if len(chars) == 5:
+            while chars[-1] == "U":
+                chars.pop()
+
+            chars[-1] = next_chars[chars[-1]]
+        else:
+            chars.append("A")
+
+        count += 1
+
+    return count

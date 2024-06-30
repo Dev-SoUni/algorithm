a16b82d70e9798a76e65e5c0bf8fb1d2d3d02a5a
Byun SoYun <bsy1482@naver.com>
2024-06-30 13:26:53 +0900

    🔥

---
+++ Python/programmers/12900_2xn 타일링_solution.py
@@ -0,0 +1,9 @@
+def solution(n):
+    arr = [0] * (n + 1)
+    arr[1] = 1
+    arr[2] = 2
+
+    for i in range(3, n + 1):
+        arr[i] = (arr[i - 2] + arr[i - 1]) % 1000000007
+
+    return arr[n]

ba5169c0112f932109ec12c35648c48a416222fb
Byun SoYun <bsy1482@naver.com>
2024-07-24 16:27:41 +0900

    🔥

---
+++ Python/programmers/42898_등굣길/solution.py
@@ -0,0 +1,12 @@
+def solution(m, n, puddles):
+    map = [[0] * (m + 1) for i in range(n + 1)]
+    map[1][1] = 1
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if i == 1 and j == 1: continue
+            if [j, i] not in puddles:
+                map[i][j] = (map[i - 1][j] + map[i][j - 1]) % 1000000007
+            else:
+                map[i][j] = 0
+    return map[n][m]

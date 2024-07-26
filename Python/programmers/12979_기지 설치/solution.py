89a4cf6744a4b341fff58a1f9883391df6050980
Byun SoYun <bsy1482@naver.com>
2024-07-26 11:59:18 +0900

    🔥

--- Python/programmers/12979_기지 설치/solution.py
+++ Python/programmers/12979_기지 설치/solution.py
@@ -16,3 +16,20 @@ def solution(n, stations, w):
 
     return answer
 
+
+# 참고 풀이
+def solution2(n, stations, w):
+    answer = 0
+    range = w + w + 1
+
+    now = 1
+    for station in stations:
+        length = station - w - now
+        if length > 0:
+            answer += math.ceil(length / range)
+        now = station + w + 1
+
+    if n >= now:
+        answer += math.ceil((n - now + 1) / range)
+
+    return answer
